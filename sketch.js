2e07ca325cedb615ac627b5cd20344242baf356c
manrajsingh248 <manraj.singh248@gmail.com>
2022-09-25 12:43:28 -0400

    Created css file, created <div> element and appended it to a container div, added basic html code to the html file, including a container div tag.

---
+++ sketch.js
@@ -0,0 +1,6 @@
+const container = document.querySelector("#container");
+
+const div = document.createElement('div');
+div.classList.add('divs');
+div.style.cssText = "background-color: pink; border: 3px solid black;";
+container.appendChild(div);
