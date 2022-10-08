2d2b75d9ba97f27cc112548338020938a3f89137
manrajsingh248 <manraj.singh248@gmail.com>
2022-10-08 12:00:03 -0400

    Added functionality to the black, blue, eraser and random buttons. Did this by creating a function colorBox() which colored each box in the grid to a color, default color is black. To change the color I added the changeColor() function and onclick event so the color would change when the color buttons are clicked.

--- sketch.js
+++ sketch.js
@@ -1,14 +1,70 @@
-//query selectors
 const container = document.querySelector('#container');
+const errorText = document.querySelector("#errorText");
 
-function makeGrid (rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (i = 0; i < (rows * cols); i++) {
-    let box = document.createElement("div");
-    box.classList.add('grid-item')
-    container.appendChild(box);
-  };
-  hoverColor();
+let color = "black";
+
+function makeGrid(size){
+  let grid = document.querySelector('.grid');
+
+  grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  
+  let total = size * size;
+  for( let i = 0; i < total; i++) {
+    let box = document.createElement('div');
+    box.addEventListener("mouseover", colorBox);
+    
+    box.style.backgroundColor = 'white';
+
+    grid.appendChild(box);
+  }
 }
+
+makeGrid(16);
+function changeSize(input) {
+  if(input >=2 && input <=100) {
+    makeGrid(input);
+  } 
+  else {
+    errorText.textContent = "Error. Please try a different size";
+  }
+}
+
+function randomColor() {
+  let letters = '0123456789ABCDEF';
+  let color2 = '#';
+  for (let i = 0; i < 6; i++) {
+    color2 += letters[Math.floor(Math.random() * 16)];
+  }
+  return color2;
+}
+
+function colorBox() {
+  if(color == "random") {
+    this.style.backgroundColor = randomColor();
+  }
+  else {
+   this.style.backgroundColor = color;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+// function makeGrid (rows, cols) {
+//   container.style.setProperty('--grid-rows', rows);
+//   container.style.setProperty('--grid-cols', cols);
+//   for (i = 0; i < (rows * cols); i++) {
+//     let box = document.createElement("div");
+//     box.classList.add('grid-item')
+//     container.appendChild(box);
+//   };
+//   hoverColor();
+// }
 
